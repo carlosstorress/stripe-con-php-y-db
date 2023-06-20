c8fd14b308cfeb9c8eb004c8dd696528b09bc64c
carlosstorress <131789627+carlosstorress@users.noreply.github.com>
2023-06-19 18:00:45 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
  
-// Product Details  
+// Establecer los detalles del producto  
 // Minimum amount is $0.50 US  
 $productName = "Pago de colegiatura";
 $descriptionName = "Pago de colegiatura del mes de Agosto";
@@ -11,16 +11,14 @@ $mainPage = 'http://localhost/stripe_checkout_con_php/payment-success.php';
 
   
 /* 
- * Stripe API configuration 
- * Remember to switch to your live publishable and secret key in production! 
- * See your keys here: https://dashboard.stripe.com/account/apikeys 
+ *Configurar las claves secretas y publicas de stripe
  */ 
 define('STRIPE_API_KEY', 'sk_test_51MgqzbFsLXR5H817qiFBGL4OixLEEiAWKPPtuOAPUCzTdGM7ZowHxz4WPOgPbr2gJ5v6thOSJxJ25BwYimGHGNvz00nGyNOPTx'); 
 define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51MgqzbFsLXR5H817ERftCB8k0o5EQwZ2O1kZIVGMowZ8IHwynOLZBu36kl6uD0DBglSypRzQYecgSqWfvs0TuTRh000YGNf2Id');
 define('STRIPE_SUCCESS_URL', $mainPage); //Payment success URL 
 define('STRIPE_CANCEL_URL', 'http://localhost/stripe_checkout_con_php/payment-cancel.php'); //Payment cancel URL 
     
-// Database configuration    
+// Configuraciin de la base de datos   
 define('DB_HOST', 'localhost');   
 define('DB_USERNAME', 'root'); 
 define('DB_PASSWORD', '');   
